b6e106b25bad7bf162abd8714dc82373d9050e45
wanglian <1521317185@qq.com>
2018-12-06 12:05:33 +0800

    增加日志依赖

--- src/main/java/com/airocov/dockerdemo1/controller/HelloWorld.java
+++ src/main/java/com/airocov/dockerdemo1/controller/HelloWorld.java
@@ -1,13 +1,16 @@
 package com.airocov.dockerdemo1.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorld {
+    private  static Logger logger = Logger.getLogger(HelloWorld.class);
     @RequestMapping("/sayhello")
     public String sayHello(){
+        logger.info("访问sayHello"+System.getProperty("user.dir"));
         return "sayhello";
     }
 }
